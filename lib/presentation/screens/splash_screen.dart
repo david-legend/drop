76d573846822495a3d325774c328606142db3340
David <davidcobbina47@gmail.com>
2020-12-31 12:35:21 +0200

    added placeholder logo for splash screen

--- lib/presentation/screens/splash_screen.dart
+++ lib/presentation/screens/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:drop/presentation/routes/router.gr.dart';
+import 'package:drop/presentation/widgets/spaces.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -11,11 +12,11 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-//    run();
+    run();
   }
 
   void run() {
-    Future.delayed(Duration(milliseconds: 1500), () {
+    Future.delayed(Duration(milliseconds: 1000), () {
       ExtendedNavigator.root.pushAndRemoveUntil(
         Routes.authScreen,
         (Route<dynamic> route) => false,
@@ -26,7 +27,27 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "Logo Goes Here",
+              style: Theme.of(context).textTheme.headline5,
+            ),
+            SpaceH16(),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 75),
+              child: Center(
+                  child: Placeholder(
+                fallbackWidth: 150,
+                fallbackHeight: 150,
+              )),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
